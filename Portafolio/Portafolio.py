5d05edb928483d12f27e8b16e54010f7530c8cf6
DanielOrias <77802157+DanielOrias@users.noreply.github.com>
2026-06-12 02:27:42 -0600

    Update Portafolio.py

--- Portafolio/Portafolio.py
+++ Portafolio/Portafolio.py
@@ -4,9 +4,3 @@ st.title("Portafolio de proyetos")
 st.header("Daniel Orias Rojas")
 st.container(height=30, border=False)
 st.subheader("**Proyecto:** :green[Color spectral matching]")
-if st.button("Información del proyecto"):
-    st.switch_page("pages/informacion_colorimetria.py")
-
-st.link_button("Código del proyecto", "https://github.com")
-
-st.link_button("Demo del proyecto", "https://github.com")
