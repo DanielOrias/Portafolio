c5fb43fd6258b8b38814c52eb0ae4efb6873b2a3
DanielOrias <77802157+DanielOrias@users.noreply.github.com>
2026-06-12 02:47:18 -0600

    Update informacion_colorimetria.py

--- pages/informacion_colorimetria.py
+++ pages/informacion_colorimetria.py
@@ -1,4 +1,16 @@
 import streamlit as st
+#----------
+with open("pages/InformacionColorimetria.pdf", "rb") as f:
+    st.download_button(
+        "Abrir PDF",
+        f,
+        file_name="InformacionColorimetria.pdf"
+    )
+
+
+
+
+#---------------
 st.title("Información del Proyecto")
 st.pdf("pages/InformacionColorimetria.pdf", height=800)
 if st.button("⬅ Volver al Inicio"):
