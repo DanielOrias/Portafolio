b517dd5e3542486272b0a89fded5aa9398f90efc
DanielOrias <77802157+DanielOrias@users.noreply.github.com>
2026-06-12 02:40:19 -0600

    Update informacion_colorimetria.py

--- pages/informacion_colorimetria.py
+++ pages/informacion_colorimetria.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Información del Proyecto")
-st.pdf("InformacionColorimetria.pdf", height=800)
+st.pdf("pages/InformacionColorimetria.pdf", height=800)
 if st.button("⬅ Volver al Inicio"):
     st.switch_page("../Portafolio.py")
