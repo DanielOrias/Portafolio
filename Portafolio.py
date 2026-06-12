854d4c1ed9161d43f18d0955539da54eb5772b92
DanielOrias <77802157+DanielOrias@users.noreply.github.com>
2026-06-12 03:40:50 -0600

    Update Portafolio.py

--- Portafolio.py
+++ Portafolio.py
@@ -1,12 +1,26 @@
 import streamlit as st
 
 st.title("Portafolio de proyetos")
+
 st.header("Daniel Orias Rojas")
+
+st.info("""Información de contacto:
+
+**Correo electrónico:** danieloriasr@gmail.com
+
+**Teléfono de contacto: +506 6481-4565**""")
+st.markdown("#### LinkedIn:")
+st.page_link("https://www.linkedin.com/in/daniel-orias-2a4779410", label="**:blue[Daniel Orias | LinkedIn.com]**")
+
 st.container(height=30, border=False)
+
+
 st.subheader("**Proyecto:** :green[Color spectral matching]")
 if st.button("Información del proyecto"):
     st.switch_page("pages/informacion_colorimetria.py")
 
+
 st.link_button("Código del proyecto", "https://github.com")
+
 
 st.link_button("Demo del proyecto", "https://github.com")
